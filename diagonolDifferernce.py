4fed89be124ec9cb6e8caecfdc29057d8ef14e49
money00233 <manikhanta0023@gmail.com>
2018-10-02 19:19:21 -0400

    Update diagonolDifferernce.py

--- diagonolDifferernce.py
+++ diagonolDifferernce.py
@@ -1,9 +1,33 @@
-def fibCheck(n):
-    if(n<=1):
-        print(n)
-    else:
-        return fibCheck(n-1) + fibCheck(n-2)
-    
+#!/bin/python3
 
+import math
+import os
+import random
+import re
+import sys
 
-fibCheck(10)
+# Complete the diagonalDifference function below.
+def diagonalDifference(arr):
+    rl_diagonol = 0
+    lr_diagonol = 0
+    for i in range(0,n):
+                lr_diagonol += arr[i][i]
+                rl_diagonol += arr[i][n-i-1]
+    test_diff = abs(lr_diagonol - rl_diagonol)
+    return test_diff
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
